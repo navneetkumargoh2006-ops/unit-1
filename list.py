385c4455915f4217db72d171d43e719f154d7ade
navneetkumargoh2006-ops <navneetkumargoh2006@gmail.com>
2026-04-01 12:14:09 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+my_list = [10, 20, 30, 40]
+
+print("First element:", my_list[0])
+
+my_list.append(50)
+print("After adding:", my_list)
+
+my_list.remove(20)
+print("After removing:", my_list)
+
+my_list.sort()
+print("Sorted list:", my_list)
+
+my_list.reverse()
+print("Reversed list:", my_list)
